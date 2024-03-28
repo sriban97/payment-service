9436e0a0f0d933e8cb7c6f81aea3057e935b7288
sriban97 <sriban.mariyappan@gmail.com>
2024-03-28 22:58:05 +0530

    add constant prop

---
+++ src/main/java/com/green/paymentservice/util/Constant.java
@@ -0,0 +1,9 @@
+package com.green.paymentservice.util;
+
+public interface Constant {
+    interface Payment{
+        String TOPIC_CREATE_PAYMENT = "create-payment";
+        String GROUP_PAYMENT = "payment-group";
+    }
+
+}
