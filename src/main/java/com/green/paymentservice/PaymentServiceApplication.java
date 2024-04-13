ebf13b7113686fec5ee48eeacc3dd4820abb6acf
sriban97 <sriban.mariyappan@gmail.com>
2024-04-13 20:13:45 +0530

    splunk log config

--- src/main/java/com/green/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/green/paymentservice/PaymentServiceApplication.java
@@ -2,9 +2,10 @@ package com.green.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 @EnableFeignClients
 public class PaymentServiceApplication {
 
